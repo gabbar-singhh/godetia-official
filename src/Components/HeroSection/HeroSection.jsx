7686a53512febb989f07490bc6f85144cf40487f
gabbar-singhh <himanshup1308@gmail.com>
2025-01-31 12:42:46 +0530

    unused img removed | hero section -done

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -5,35 +5,46 @@ import Image from "next/image";
 const HeroSection = () => {
   const images = [
     {
-      src: "/assets/hero_mehandi.png",
-      alt: "lady with mehandi in hand img",
+      src: "/assets/gallery1.png",
+      alt: "1",
       column: 1,
     },
     {
-      src: "/assets/hero_chef.png",
-      alt: "indian chef img",
+      src: "/assets/gallery2.png",
+      alt: "2",
       column: 1,
     },
     {
-      src: "/assets/hero_photographer.png",
-      alt: "woman with a tripod & cam",
+      src: "/assets/gallery3.png",
+      alt: "3",
       column: 2,
     },
     {
-      src: "/assets/hero_makeup.png",
-      alt: "makeup artist img",
+      src: "/assets/gallery4.png",
+      alt: "4",
       column: 2,
     },
   ];
   return (
     <section className={styles.main}>
       <div className={styles.content}>
+        <p className={styles.chip}>
+          <Image
+            src="/icons/checkmark-chip.svg"
+            height={24}
+            width={24}
+            alt="checkmark icon"
+          />
+          Globally Certified
+        </p>
         <h1>
           Your Premier Partner for{" "}
           <span className={styles.maroon}> First Aid Solutions.</span>
         </h1>
         <p>
-        With 10+ years of expertise in manufacturing and exporting first aid kits, we offer OEM/ODM solutions tailored to your brand, delivering certified products to keep you safe and prepared
+          With 10+ years of expertise in manufacturing and exporting first aid
+          kits, we offer OEM/ODM solutions tailored to your brand, delivering
+          certified products to keep you safe and prepared.
         </p>
         <div className={styles.ctaButton}>
           <span>Reach out on WhatsApp</span>
@@ -52,14 +63,24 @@ const HeroSection = () => {
             {images
               .filter((image) => image.column === 1)
               .map((image, index) => (
-                <img key={index} src={image.src} alt={image.alt} />
+                <img
+                  key={index}
+                  src={image.src}
+                  alt={image.alt}
+                  className={styles[`img_${image.alt}`]}
+                />
               ))}
           </div>
           <div className={styles.galleryGridCol_2}>
             {images
               .filter((image) => image.column === 2)
               .map((image, index) => (
-                <img key={index} src={image.src} alt={image.alt} />
+                <img
+                  key={index}
+                  src={image.src}
+                  alt={image.alt}
+                  className={styles[`img_${image.alt}`]}
+                />
               ))}
           </div>
         </div>
