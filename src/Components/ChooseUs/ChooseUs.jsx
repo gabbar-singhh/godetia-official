a7b087f2cb4b1ac223a1bb045496ce83d26d2638
gabbar-singhh <himanshup1308@gmail.com>
2025-02-01 11:31:48 +0530

    choose us -done

--- src/Components/ChooseUs/ChooseUs.jsx
+++ src/Components/ChooseUs/ChooseUs.jsx
@@ -8,8 +8,8 @@ const ChooseUs = () => {
         Why People Trust <span className={styles.godetia}>Godetia?</span>
       </h1>
 
-      <div className={styles.cardStack}>
-        <div className={styles.card}>
+      <ul className={styles.cardStack}>
+        <li className={styles.card}>
           <img src="/icons/star.svg" height={62} alt="star image" />
           <h2>High quality, no cost cutting</h2>
           <p>
@@ -17,17 +17,17 @@ const ChooseUs = () => {
             quality checks, ensuring our product is safe and effective. We never
             compromise with your health.
           </p>
-        </div>{" "}
-        <div className={styles.card}>
-          <img src="/icons/handshake.svg" height={62} alt="handshake image" />
+        </li>{" "}
+        <li className={styles.card}>
+          <img src="/icons/handshake.svg" height={56} alt="handshake image" />
           <h2>We Understand your needs</h2>
           <p>
             We understand every client has unique needs, so we work closely to
             create products that fit your brand. Our OEM/ODM solutions ensure
             they meet your exact requirements.
           </p>
-        </div>{" "}
-        <div className={styles.card}>
+        </li>{" "}
+        <li className={styles.card}>
           <img src="/icons/shield.svg" height={62} alt="shield image" />
           <h2>Trusted Kits. Anytime. Anywhere</h2>
           <p>
@@ -35,8 +35,8 @@ const ChooseUs = () => {
             checks, ensuring it performs reliably in critical situations when
             you need it most.
           </p>
-        </div>
-      </div>
+        </li>
+      </ul>
     </section>
   );
 };
